ab8a7f77864d6798f41fd4add91efb1dc6f0fd3e
Alina M <crlin.m@gmail.com>
2026-03-08 23:39:37 +0900

    feat: start restore function (read snap-file, create newfolder, etc

--- src/fs/restore.js
+++ src/fs/restore.js
@@ -1,8 +1,34 @@
+import path from 'node:path';
+import fsp from 'node:fs/promises';
+
+const rootPath = path.join(import.meta.dirname);
+const snapshotPath = path.join(rootPath, '../../snapshot.json');
+const workspace_restored = path.join(rootPath, '../../workspace_restored');
+
 const restore = async () => {
-  // Write your code here
-  // Read snapshot.json
-  // Treat snapshot.rootPath as metadata only
-  // Recreate directory/file structure in workspace_restored
+  let structure;
+
+  try {
+    const snap = await fsp.readFile(snapshotPath, {encoding: 'utf8'});
+    structure = JSON.parse(snap);
+    console.log(structure);
+  } catch {
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    await fsp.mkdir(workspace_restored);
+  } catch {
+    throw new Error("FS operation failed");
+  }
+
+  try {
+    // for (item of structure) {
+    //   console.log('item:', item);
+    // }
+  } catch {
+    throw new Error("FS operation failed");
+  }
 };
 
 await restore();
